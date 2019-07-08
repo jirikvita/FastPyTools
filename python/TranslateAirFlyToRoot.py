d6d886548d47b2f2ff30189826723d3fd170e308
Jiri Kvita <jiri.kvita@upol.cz>
2019-07-08 15:51:28 +0200

    dvl

--- python/TranslateAirFlyToRoot.py
+++ python/TranslateAirFlyToRoot.py
@@ -6,14 +6,19 @@ from __future__ import print_function
 
 import os, sys
 
-indir='/data/FAST/Auger/'
-outdir='/data/FAST/Auger-ROOT/'
+# Auger:
+#indir='/data/FAST/Auger/'
+#outdir='/data/FAST/Auger_ROOT/'
+
+#TA:
+indir='/data/FAST/TA/'
+outdir='/data/FAST/TA_ROOT/'
 
 for lrun in os.popen('cd {} ; ls | grep run'.format(indir)):
     run = lrun[:-1]
     #print(run)
     os.system('mkdir -p {}/{}'.format(outdir, run))
-    for linfile in os.popen('ls {}/{}/*.data.part*'.format(indir, run)):
+    for linfile in os.popen('ls {}/{}/*.data*'.format(indir, run)):
         infile = linfile[:-1]
         #print('  {}'.format(infile))
         outfile = infile
